23785ae0e6aa0d57d11fa59366fa5078e85d3847
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2016-11-14 16:35:29 +0100

    Variables

--- js/js.js
+++ js/js.js
@@ -1,6 +1,12 @@
 // alert("working");
 
-document.write("<h1>document.write</h1><hr>");
+//////////////////////////////////////////////////////////////////////////////////
+
+document.write("<h1>JavaScript for Beginners</h1><hr>");
+
+////////////////////////////////////////////////////////////////////////////////// 
+
+document.write("<h2>document.write</h2><hr>");
 
 // Local
 
@@ -15,7 +21,8 @@ function hello(name) {
 hello(name);
 console.log(hello);
 
-document.write("<hr>");
+//////////////////////////////////////////////////////////////////////////////////
+document.write("<br>");
 
 // Global
 
@@ -29,3 +36,50 @@ function hello(name) {
 }
 
 hello(name);
+
+//////////////////////////////////////////////////////////////////////////////////
+document.write("<hr>");
+
+document.write("<h2>Variables</h2>");
+document.write("<hr>");
+
+document.write("<h3>Number</h3>");
+
+var myNumber = 16;
+
+myNumber = myNumber + 5;
+
+document.write(myNumber);
+console.log(typeof myNumber);
+
+document.write("<h3>String</h3>");
+
+var completeName = "Soresini";
+
+completeName = completeName + " " + "Matteo";
+
+document.write(completeName);
+console.log(typeof completeName);
+
+document.write("<h3>Booleans</h3>");
+
+var readyToWork = true;
+var notReady = false;
+
+document.write(readyToWork);
+document.write("<br>");
+document.write(notReady);
+console.log(typeof notReady);
+
+
+
+
+
+
+
+
+
+
+
+
+
