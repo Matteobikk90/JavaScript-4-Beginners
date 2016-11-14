26c943e357a55a2f6807d3d4b4ceb46898b6693b
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2016-11-14 16:15:23 +0100

    document.write

--- js/js.js
+++ js/js.js
@@ -1,1 +1,31 @@
 // alert("working");
+
+document.write("<h1>document.write</h1><hr>");
+
+// Local
+
+document.write("Local: ");
+
+function hello(name) {
+	var name = "Matteo";
+	console.log("Hi, " + name);
+	document.write("Hi, " + name);
+}
+
+hello(name);
+console.log(hello);
+
+document.write("<hr>");
+
+// Global
+
+document.write("Global: ");
+
+var name = "Matteo";
+
+function hello(name) {
+	console.log("Hi, " + name);
+	document.write("Hi, " + name);
+}
+
+hello(name);
